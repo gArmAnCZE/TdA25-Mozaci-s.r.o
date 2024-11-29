2df6e861f1f6fa5b2bea3473f072516be419590f
Payrifunky <79419678+Payrifunky@users.noreply.github.com>
2024-11-29 12:35:34 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -18,13 +18,13 @@ except OSError:
 db.init_app(app)
 
 
-#@app.route('/')
-#def hello_world():  # put application's code here
-#    return "Hello TdA!"
+@app.route('/')
+def hello_world():  # put application's code here
+    return "Hello TdA!"
 
-#@app.route('/api')
-#def api():  # put application's code here
-#    return jsonify({"organization": "Student Cyber Games"})
+@app.route('/api')
+def api():  # put application's code here
+    return jsonify({"organization": "Student Cyber Games"})
 
 
 if __name__ == '__main__':
